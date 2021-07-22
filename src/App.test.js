612ce427f6d5c25120c1bd58d7b17077769a1b09
sushilkgeneral <sushilkgeneral@gmail.com>
2021-07-22 12:00:13 -0400

    Test suite update

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders the root App component", () => {
-  render(<App />);
-  const element = screen.queryByTestId("tic-tac-toe-app");
-  expect(element).toBeInTheDocument();
+describe('App Component', () => {
+  test("renders the element with 'tic-tac-toe-app' as test Id", () => {
+    render(<App />);
+    const element = screen.queryByTestId("tic-tac-toe-app");
+    expect(element).toBeInTheDocument();
+  });
 });
+

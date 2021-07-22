7e42c8e0b4aac51cc9901644e4829e77ed2965fe
sushilkgeneral <sushilkgeneral@gmail.com>
2021-07-22 10:34:44 -0400

    App component test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the root App component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.queryByTestId('tic-tac-toe-app')
+  expect(element).toBeInTheDocument();
 });

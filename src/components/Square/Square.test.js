911f7cc92f9f9696ba59d9622ec6dee35103d3b0
sushilkgeneral <sushilkgeneral@gmail.com>
2021-07-22 19:41:19 -0400

    Testing for the value prop to be rendered

--- src/components/Square/Square.test.js
+++ src/components/Square/Square.test.js
@@ -1,11 +1,19 @@
-import {render, screen} from "@testing-library/react";
+import {cleanup, render, screen} from "@testing-library/react";
 import Square from "./Square";
 
-describe('Square Component', () => {
-    test('', () => {
-        render(<Square/>);
+afterEach(cleanup);
 
+describe('Square Component', () => {
+    test('renders a button', () => {
+        render(<Square/>);
         const button = screen.queryByRole("button");
+        expect(button).toBeInTheDocument();
+    });
+
+    test('renders the value passed through the props', () => {
+        render(<Square value='X'/>);
+
+        const button = screen.queryByText('X');
         expect(button).toBeInTheDocument();
     });
 });

a1cbc5a9208b204f1072bc462a9c8ed889c14706
sushilkgeneral <sushilkgeneral@gmail.com>
2021-07-22 19:02:06 -0400

    Added test to check for the heading element

--- src/components/Board/Board.test.js
+++ src/components/Board/Board.test.js
@@ -2,9 +2,13 @@ import {render, screen} from "@testing-library/react";
 import Board from './Board';
 
 describe('Board component', () => {
-    test('renders a component with test id "board"', () => {
+    test('renders a component with the right elements', () => {
         render(<Board />);
-        const element = screen.queryByTestId("board");
-        expect(element).toBeInTheDocument();
+
+        const mainElement = screen.queryByTestId("board");
+        expect(mainElement).toBeInTheDocument();
+
+        const headingElement = screen.queryByText("Tic Tac Toe")
+        expect(headingElement).toBeInTheDocument();
     });
 });

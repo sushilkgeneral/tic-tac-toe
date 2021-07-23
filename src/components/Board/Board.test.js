b73537935ee0cc40bb9815d50123b8f7d33ccfde
sushilkgeneral <sushilkgeneral@gmail.com>
2021-07-23 00:42:27 -0400

    Testing Square clicks on the board

--- src/components/Board/Board.test.js
+++ src/components/Board/Board.test.js
@@ -1,6 +1,8 @@
-import { screen} from "@testing-library/react";
+import {cleanup, fireEvent, screen} from "@testing-library/react";
 import { customRender } from "../../test-utils";
 import Board from './Board';
+
+afterEach(cleanup);
 
 describe('Board component', () => {
     test('renders with the correct elements', () => {
@@ -15,4 +17,30 @@ describe('Board component', () => {
         const squares = screen.queryAllByTestId('square');
         expect(squares.length).toBe(9);
     });
+
+    test('clicking on an empty square updates the value & switches player', () => {
+        customRender(<Board />);
+        const squares = screen.queryAllByTestId('square');
+        expect(screen.queryAllByText('X').length).toBe(0);
+        expect(screen.queryAllByText('O').length).toBe(0);
+        fireEvent.click(squares[0]);
+        expect(screen.queryAllByText('X').length).toBe(1);
+        expect(screen.queryAllByText('O').length).toBe(0);
+        fireEvent.click(squares[1]);
+        expect(screen.queryAllByText('X').length).toBe(1);
+        expect(screen.queryAllByText('O').length).toBe(1);
+    })
+
+    test('clicking on an non-empty square does not update values & does not switches player', () => {
+        customRender(<Board />);
+        const squares = screen.queryAllByTestId('square');
+        expect(screen.queryAllByText('X').length).toBe(0);
+        expect(screen.queryAllByText('O').length).toBe(0);
+        fireEvent.click(squares[3]);
+        expect(screen.queryAllByText('X').length).toBe(1);
+        expect(screen.queryAllByText('O').length).toBe(0);
+        fireEvent.click(squares[3]);
+        expect(screen.queryAllByText('X').length).toBe(1);
+        expect(screen.queryAllByText('O').length).toBe(0);
+    })
 });

24fba3fd561cfdd32a49066aaaf26085631e2330
sushilkgeneral <sushilkgeneral@gmail.com>
2021-07-23 00:51:08 -0400

    Test suite for a new Button component

---
+++ src/components/Button/Button.test.js
@@ -0,0 +1,26 @@
+import {render} from "../../test-utils";
+import Button from "./Button";
+import {fireEvent, screen} from "@testing-library/react";
+
+describe('Button Component', () => {
+    test('renders a button', () => {
+        render(<Button/>);
+        const button = screen.queryByRole("button");
+        expect(button).toBeInTheDocument();
+    });
+
+    test('renders the value passed through the props', () => {
+        render(<Button value='reset'/>);
+
+        const button = screen.queryByText('reset');
+        expect(button).toBeInTheDocument();
+    });
+
+    test('when button is clicked - calls the onClick function passed via props', () => {
+        const handleClick = jest.fn()
+        render(<Button onClick={handleClick}/>);
+
+        fireEvent.click(screen.getByRole("button"));
+        expect(handleClick).toHaveBeenCalledTimes(1)
+    });
+});

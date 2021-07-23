9239787b337d63d718c077b743b62fb47d0e66f5
sushilkgeneral <sushilkgeneral@gmail.com>
2021-07-23 03:01:49 -0400

    Hooked up the calculator reducer to the board

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -3,10 +3,17 @@ import Square from "../Square/Square";
 import classes from "./Board.module.css";
 import { actions } from "../../store/store";
 import Button from "../Button/Button";
+import { useEffect } from "react";
 
 const Board = () => {
     const dispatch = useDispatch();
     const values = useSelector(state => state.values);
+    const history = useSelector(state => state.history);
+    const winner = useSelector(state => state.winner);
+
+    useEffect(() => {
+        dispatch(actions.calculateWinner(values));
+    }, [dispatch, values]);
 
     const rewindHandler = () => {
         dispatch(actions.handleRewind());
@@ -34,6 +41,8 @@ const Board = () => {
                 <Button onClick={rewindHandler} text="Rewind"/>
                 <Button onClick={resetHandler} text="Reset"/>
             </div>
+            {winner && <p>{`${winner} wins!`}</p>}
+            {!winner && history.length === 10 && <p>Its a tie</p>}
         </div>
     );
 }
